ee0df2c8b16dc6e6e35ec297a7be181efb777efb
fifthmarauder <eeshashahbaz12345@gmail.com>
2026-05-11 18:15:04 +0500

    initial commit

---
+++ src/service/user.service.ts
@@ -0,0 +1,12 @@
+interface User {
+  name?: string;
+  email: string;
+  password: string;
+}
+export const registerUser = async ({ name, email, password }: User) => {
+  if (!name || !email || !password) {
+    throw new Error("Missing required fields");
+  }
+
+  const existing = 
+};
