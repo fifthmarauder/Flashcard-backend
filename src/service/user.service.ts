37ab3536e265b254c17074d507261b7dcab743ce
fifthmarauder <eeshashahbaz12345@gmail.com>
2026-05-15 00:44:47 +0500

    schema updates

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -7,6 +7,5 @@ export const registerUser = async ({ name, email, password }: User) => {
   if (!name || !email || !password) {
     throw new Error("Missing required fields");
   }
-
-  
+  const existing = await UserModel;
 };
